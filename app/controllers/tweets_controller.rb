6c54cdf8bc5af158b077903473ad932e39bef13c
unknown <>
2017-04-27 22:43:34 +0900

    ログイン機能完成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,9 @@
 class TweetsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
-    @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(1)
+    @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(3)
   end
 
   def new
@@ -12,6 +14,9 @@ class TweetsController < ApplicationController
   end
 
   private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
   def tweet_params
     params.permit(:name, :image, :text)
   end
